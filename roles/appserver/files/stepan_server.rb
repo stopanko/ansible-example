1ccd678295da29518c48ed89fb97506630169dd2
stepan.boichyshyn <stepan.boichyshyn@travelport.com>
2019-02-27 13:15:55 +0200

    Refactor structure and fix nginx configuration issue

--- roles/appserver/files/stepan_server.rb
+++
@@ -1,7 +0,0 @@
-require 'socket'
-server = TCPServer.new 5678
-
-while session = server.accept
-  session.puts "StepanServer! The time is #{Time.now}"
-  session.close
-end
---
+++ roles/appserver/templates/stepan_server.rb
@@ -0,0 +1,21 @@
+require 'socket'
+server = TCPServer.new 5678
+
+while session = server.accept
+  request = session.gets
+  puts request
+
+  session.print "HTTP/1.1 200\r\n" # 1
+  session.print "Content-Type: text/html\r\n" # 2
+  session.print "\r\n" # 3
+  session.print "StepanServer! The time is #{Time.now}. Sent from {{ansible_play_hosts[0]}}"
+
+  session.close
+end
+
+# require 'rack'
+# app = Proc.new do |env|
+#     ['200', {'Content-Type' => 'text/html'}, ["StepanServer! The time is #{Time.now}"]]
+# end
+
+# Rack::Handler::WEBrick.run app, :Port => 5678
