0d6f158ba766febd7fd0eb3a0336333fecea7bb8
stepan.boichyshyn <stepan.boichyshyn@travelport.com>
2019-03-15 13:55:01 +0200

    Refactor nginx configuration

--- roles/appserver/templates/stepan_server.rb
+++ roles/appserver/templates/stepan_server.rb
@@ -8,6 +8,8 @@ while session = server.accept
   session.print "HTTP/1.1 200\r\n" # 1
   session.print "Content-Type: text/html\r\n" # 2
   session.print "\r\n" # 3
+  # Add mac address to the output
+  # session.print "StepanServer! The time is #{Time.now}. Sent from #{x('ls')}"
   session.print "StepanServer! The time is #{Time.now}. Sent from {{ansible_play_hosts[0]}}"
 
   session.close
