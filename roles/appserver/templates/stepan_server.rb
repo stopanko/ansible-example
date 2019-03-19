7f133e350b0b30bf6d73a024af8feed28b595784
stepan.boichyshyn <stepan.boichyshyn@travelport.com>
2019-03-19 18:55:09 +0200

    Replace ruby app with rack

--- roles/appserver/templates/stepan_server.rb
+++ roles/appserver/templates/stepan_server.rb
@@ -1,23 +1,23 @@
-require 'socket'
-server = TCPServer.new 5678
+# require 'socket'
+# server = TCPServer.new 5678
 
-while session = server.accept
-  request = session.gets
-  puts request
+# while session = server.accept
+#   request = session.gets
+#   puts request
 
-  session.print "HTTP/1.1 200\r\n" # 1
-  session.print "Content-Type: text/html\r\n" # 2
-  session.print "\r\n" # 3
-  # Add mac address to the output
-  # session.print "StepanServer! The time is #{Time.now}. Sent from #{x('ls')}"
-  session.print "StepanServer! The time is #{Time.now}. Sent from {{ansible_play_hosts[0]}}"
+#   session.print "HTTP/1.1 200\r\n" # 1
+#   session.print "Content-Type: text/html\r\n" # 2
+#   session.print "\r\n" # 3
+#   # Add mac address to the output
+#   # session.print "StepanServer! The time is #{Time.now}. Sent from #{x('ls')}"
+#   session.print "StepanServer! The time is #{Time.now}. Sent from {{ansible_play_hosts[0]}}"
 
-  session.close
-end
-
-# require 'rack'
-# app = Proc.new do |env|
-#     ['200', {'Content-Type' => 'text/html'}, ["StepanServer! The time is #{Time.now}"]]
+#   session.close
 # end
 
-# Rack::Handler::WEBrick.run app, :Port => 5678
+require "rack"
+app = Proc.new do |env|
+    ['200', {'Content-Type' => 'text/html'}, ["StepanServer! The time is #{Time.now}. (c) Rack server"]]
+end
+
+Rack::Handler::WEBrick.run app, :Port => 5678
